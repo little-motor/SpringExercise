0563857d40d0d57580fb832ec894232843f250b8
LittleMotor <479217391@qq.com>
2018-05-26 12:08:17 +0800

    setter方法依赖注入

--- springExercise/src/main/java/helloWorld/Main.java
+++ springExercise/src/main/java/helloWorld/Main.java
@@ -5,10 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
   public static void main(String[] args) {
-    //1、创建Spring的IOC容器的对象
-    ApplicationContext ctx = new ClassPathXmlApplicationContext("Beans.xml");
-    //2、从IOC的容器中获取Bean的实例
-    HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloworld");
+    //1、创建Spring的IoC容器的对象
+    ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+    //2、从IoC的容器中获取Bean的实例
+    HelloWorld helloWorld = (HelloWorld) context.getBean("helloworld");
     //3、调用hello方法
     helloWorld.hello();
 }
---
+++ springExercise/src/main/java/setter/Address.java
@@ -0,0 +1,18 @@
+package setter;
+
+public class Address {
+  private String city;
+  private String country;
+  
+  public Address(String city, String country) {
+    this.city = city;
+    this.country = country;
+  }
+  
+  @Override
+  public String toString() {
+    return "city: " + city + "country: " + country;
+  }
+  
+  
+}
